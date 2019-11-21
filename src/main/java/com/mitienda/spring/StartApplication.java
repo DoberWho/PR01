9bc64373d78bca3559c2760fa77c4e138456619f
DoberWho <jorge.parada@ctbarbanza.com>
2019-11-21 12:32:19 +0100

    .

--- src/main/java/com/mitienda/spring/StartApplication.java
+++ src/main/java/com/mitienda/spring/StartApplication.java
@@ -1,5 +1,7 @@
 package com.mitienda.spring;
 
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
@@ -7,6 +9,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.mitienda.spring.menu.menuPrincipal;
+import com.mitienda.spring.models.Categoria;
+import com.mitienda.spring.repositories.CategoriasRepository;
 
 @SpringBootApplication
 public class StartApplication extends menuPrincipal implements CommandLineRunner {
@@ -24,7 +28,7 @@ public class StartApplication extends menuPrincipal implements CommandLineRunner
 		/**
 		 * Aqui controlar los Menus y el cambio entre ellos. Y En cada menu; usar su
 		 * respectivo Controller y Repository
-		 */
+		 */ 
 
 		menuPrincipal.iniciaMenu();
 
